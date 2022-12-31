f195073865f0aef68796f95dc0e0fb8eb268f11a
Tom Ruff <tomruffcodes@gmail.com>
2022-12-30 21:48:39 -0700

    updated maifest and service worker

--- utils/sw.js
+++ utils/sw.js
@@ -1,5 +1,5 @@
 var cacheName = "thegospel-pwa";
-var filesToCache = ["/", "/index.html", "/assets/css/style.css","/assets/js/user.js","/assets/js/actions.js","/assets/js/fontActions.js",'/assets/data/bibleData.js'];
+var filesToCache = ["../", "../index.html", "../assets/css/style.css","../assets/js/user.js","../assets/js/actions.js","../assets/js/fontActions.js",'../assets/data/bibleData.js'];
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
